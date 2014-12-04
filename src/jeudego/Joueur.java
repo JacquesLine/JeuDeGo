5e89e2c5ef2369426efa49e50c14075fbe04dfd0
cbrandon <cbrandon@pc-b12-15.eleves.ecn>
2014-12-04 11:56:19 +0100

    Création de la méthode Jouer

--- src/jeudego/Joueur.java
+++ src/jeudego/Joueur.java
@@ -4,6 +4,9 @@
  */
 package jeudego;
 
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author cbrandon
@@ -22,6 +25,24 @@ public class Joueur {
     
     public String getCouleur(){
         return couleur;
+    }
+    
+    public Point Jouer(PlateauJeu pl){
+        Point po = new Point(0,0);
+        
+        System.out.println("Veuillez choisir une case du plateau /n x=");
+        
+        Scanner saisie=new Scanner(System.in);
+        po.setx(saisie.nextInt());
+        System.out.println("y=");
+        po.sety(saisie.nextInt());
+        
+        if (pl.suicide(po)){
+            return po;}
+        else{
+            System.out.println("Vous ne pouvez pas poser de pions sur cette case");
+            return Jouer(pl);
+        }
     }
     
 }
