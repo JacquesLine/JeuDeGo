92616b71ebc7e3abf709f20a054c92ebe3f866c7
cbrandon <cbrandon@pc-b12-15.eleves.ecn>
2014-12-15 11:23:09 +0100

    modif après merge

--- src/jeudego/PlateauJeu.java
+++ src/jeudego/PlateauJeu.java
@@ -191,7 +191,6 @@ public class PlateauJeu {
      */
     public boolean suicide(boolean b) {
         boolean res = false;
-
         return res;
     }
     
@@ -200,7 +199,8 @@ public class PlateauJeu {
      * @param A the player playing.
      * @return
      */
-/*
+    
+    /**
      * Method to determine if an precedent configuration come back
      * voisin is a local boolean which permits to not copy paste some tests
      * the fonction returns true if the ko rule is respected, if the movement is legal
@@ -237,9 +237,15 @@ public class PlateauJeu {
         }
         return true;
  }
+   
+    
+    
 
     public PlateauJeu tourDeJeu(Joueur A){
     	System.out.println("C'est au tour du joueur "+A.getCouleur()+" de jouer");
+    	boolean b= true;
+    	if (A.getCouleur().equals("blanc")){b=true;}
+    	else{b=false;}
     	PlateauJeu futur=new PlateauJeu(this);
     	A.jouer(futur,System.in);
     	if (futur.suicide()) {return futur;}
@@ -252,7 +258,7 @@ public class PlateauJeu {
     }
 
     public void miseAJour(Joueur A) {
+        
     }
   }
-        return true;    
-    }
+
